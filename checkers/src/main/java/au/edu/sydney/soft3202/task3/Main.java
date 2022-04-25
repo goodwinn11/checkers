6d8e843371d8db177746013e80f73fb4702cc45e
ivankosenko <kosenko.bmstu@gmail.com>
2022-04-25 16:54:38 +1000

    first part almost done

---
+++ checkers/src/main/java/au/edu/sydney/soft3202/task3/Main.java
@@ -0,0 +1,32 @@
+package au.edu.sydney.soft3202.task3;
+
+import au.edu.sydney.soft3202.task3.model.GameBoard;
+import au.edu.sydney.soft3202.task3.model.SQLDataBase;
+import au.edu.sydney.soft3202.task3.view.GameWindow;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private final GameBoard model = new GameBoard();
+    private final GameWindow view = new GameWindow(model);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(view.getScene());
+        SQLDataBase chess = new SQLDataBase();
+        chess.removeDB();
+       chess.createDB();
+       chess.setupDB();
+       //chess.addStartingData();
+//        addDataFromQuestionableSource("2", "none", "1,2,3,4");
+      //  chess.createDB();
+    //    SQLDataBase.setupDB();
+       // SQLDataBase.addStartingData();
+        primaryStage.setTitle("Checkers");
+        primaryStage.show();
+    }
+}
